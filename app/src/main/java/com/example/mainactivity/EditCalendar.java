7091d0ce9e4d41c9d9c62a7e55fad4a12b0f4744
Ethan <ethanmahc@gmail.com>
2020-03-18 10:04:34 -0700

    Added Event Listeners for Elements

--- app/src/main/java/com/example/mainactivity/EditCalendar.java
+++ app/src/main/java/com/example/mainactivity/EditCalendar.java
@@ -3,18 +3,30 @@ package com.example.mainactivity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.hardware.SensorEventListener;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class EditCalendar extends AppCompatActivity implements View.OnClickListener {
 
+    //elements in gridlayout 1
     private ImageView arrowRowOne;
     private Button saveButtonRowOne;
 
+    //elements in gridlayout 2
+    private EditText addTitle;
+
+    //elements in gridlayout 3
+    private TextView editDateRowOne;
+    private TextView editTimeRowOne;
+    private TextView editDateRowTwo;
+    private TextView editTimeRowTwo;
+
+    //elements in gridlayout 4
+    private EditText notificationMessage;
 
 
     @Override
@@ -22,11 +34,24 @@ public class EditCalendar extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_calendar);
 
-        //setting listeners by grid-row
-        saveButtonRowOne = (Button) findViewById(R.id.saveButtonRowOne);
+        //setting listeners in gridlayout 1
+        saveButtonRowOne = (Button) findViewById(R.id.saveButton);
         saveButtonRowOne.setOnClickListener(this);
-        arrowRowOne = (ImageView) findViewById(R.id.arrowRowOne);
+        arrowRowOne = (ImageView) findViewById(R.id.arrow);
         arrowRowOne.setOnClickListener(this);
+
+        //setting listeners in gridlayout 2
+        addTitle = (EditText) findViewById(R.id.addTitle);
+
+        //setting listeners in gridlayout 3
+        editDateRowOne = (TextView) findViewById(R.id.editDateRowOne);
+        editTimeRowOne = (TextView) findViewById(R.id.editTimeRowOne);
+
+        editDateRowTwo = (TextView) findViewById(R.id.editDateRowTwo);
+        editTimeRowTwo = (TextView) findViewById(R.id.editTimeRowTwo);
+
+        //setting listeners in gridlayout 4
+        notificationMessage = (EditText) findViewById(R.id.notificationMessage);
 
     }
 
