aea39292393e5c51c674d7c7ae3e6887891111be
Ethan <ethanmahc@gmail.com>
2020-03-17 17:28:15 -0700

    Edit Calendar Activity

--- app/src/main/java/com/example/mainactivity/MainActivity.java
+++ app/src/main/java/com/example/mainactivity/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.mainactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    // loading editcalendar button
+    private Button EditCalendarButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent i = new Intent(this,EditCalendar.class);
+        startActivity(i);
     }
 }
