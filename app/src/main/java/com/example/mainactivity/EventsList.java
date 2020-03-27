27ab83902ea342e42e8a945c3e352cf1de531d22
Thomas <thomas.tran789@gmail.com>
2020-03-26 17:27:38 -0700

    Updated drawable files

--- app/src/main/java/com/example/mainactivity/EventsList.java
+++ app/src/main/java/com/example/mainactivity/EventsList.java
@@ -2,16 +2,22 @@ package com.example.mainactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
+import androidx.core.content.res.ResourcesCompat;
 
 import android.content.Intent;
 import android.content.res.Resources;
 import android.database.Cursor;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.util.Log;
 import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toolbar;
@@ -19,6 +25,8 @@ import android.widget.Toolbar;
 import com.example.mainactivity.Database.Constants;
 import com.example.mainactivity.Database.MyDatabase;
 import com.example.mainactivity.Database.MyDatabaseHelper;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
@@ -29,12 +37,20 @@ public class EventsList extends AppCompatActivity {
     TextView hour;
     String toolbarDate;
     ScrollView scrollView;
+
     LinearLayout parentContainer;
     LinearLayout timeline;
     LinearLayout boxParent;
     LinearLayout box;
     LinearLayout.LayoutParams layoutParams;
     LinearLayout.LayoutParams layoutParams2;
+
+    //EVENT LAYOUTS WITH BOXCONTAINER
+    RelativeLayout eventsContainer;
+    FrameLayout frameLayout;
+    LinearLayout eventLayout;
+
+
     private Resources r;
     Toolbar toolbar;
     String dateClicked;
@@ -50,7 +66,9 @@ public class EventsList extends AppCompatActivity {
         setContentView(R.layout.activity_events_list);
         intent = getIntent();
         dateClicked = intent.getStringExtra("DATE_CLICKED");
-        //parser = dateClicked.split()
+        parser = dateClicked.split(" ");
+
+        //eventsContainer = findViewById(R.id.events_container);
 
         //database
         db = new MyDatabase(this);
@@ -59,8 +77,7 @@ public class EventsList extends AppCompatActivity {
         //Toolbar stuff
         toolbarDate = intent.getStringExtra("TOOLBAR");
         toolbar = findViewById(R.id.actionBar);
-        toolbarTitle= toolbar.findViewById(R.id.toolbarTitle);
-
+        toolbarTitle = toolbar.findViewById(R.id.toolbarTitle);
         toolbarTitle.setText(toolbarDate);
 
         //Scrollview containers
@@ -69,15 +86,18 @@ public class EventsList extends AppCompatActivity {
         width = Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 75, r.getDisplayMetrics()));
         height = Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 60, r.getDisplayMetrics()));
 
+
         layoutParams = new LinearLayout.LayoutParams(width, height);
         layoutParams2 = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, height);
 
+
         setContainers();
-        scrollView.addView(parentContainer);
         fillLayout();
 
-        //Display event
-        displayEvent();
+
+        scrollView.addView(parentContainer);
+
+
     }
 
     private void fillLayout(){
@@ -132,17 +152,25 @@ public class EventsList extends AppCompatActivity {
     }
 
     private void setContainers(){
+
+
+        //first child container to scrollview
         parentContainer = new LinearLayout(this);
         LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
         parentContainer.setLayoutParams(lp);
         parentContainer.setTag("parentContainer");
 
+        //FrameLayout instantiation
+        frameLayout = new FrameLayout(this);
+        frameLayout.setLayoutParams(lp);
+
+        //container to boxes
         boxParent = new LinearLayout(this);
         boxParent.setLayoutParams(lp);
         boxParent.setOrientation(LinearLayout.VERTICAL);
         boxParent.setTag("boxes");
 
-
+        //container to timeline
         timeline = new LinearLayout(this);
         lp = new LinearLayout.LayoutParams(width, LinearLayout.LayoutParams.WRAP_CONTENT);
         timeline.setLayoutParams(lp);
@@ -152,8 +180,14 @@ public class EventsList extends AppCompatActivity {
         timeline.setOrientation(LinearLayout.VERTICAL);
         timeline.setTag("timeline");
 
+
+        //add timeline and boxes to parent container
         parentContainer.addView(timeline);
-        parentContainer.addView(boxParent);
+        frameLayout.addView(boxParent);
+        displayEvent();
+        parentContainer.addView(frameLayout);
+        //Display event
+
     }
 
     //next intent
@@ -163,8 +197,10 @@ public class EventsList extends AppCompatActivity {
         startActivity(editCalendarIntent);
     }
 
+
     private void displayEvent(){
         Cursor cursor = db.getData();
+
         int index0 = cursor.getColumnIndex(Constants.DATECLICKED);
         int index1 = cursor.getColumnIndex(Constants.TITLE);
         int index2 = cursor.getColumnIndex(Constants.TIMEONE);
@@ -172,25 +208,117 @@ public class EventsList extends AppCompatActivity {
         int index4 = cursor.getColumnIndex(Constants.MESSAGE);
         int index5 = cursor.getColumnIndex(Constants.COLOR);
 
-        ArrayList<String> mArrayList = new ArrayList<>();
+        ArrayList<String[]> mArrayList = new ArrayList<>();
         cursor.moveToFirst();
+
         while(!cursor.isAfterLast()){
             String dateDatabase = cursor.getString(index0);
-            if(dateDatabase.contentEquals(dateClicked)) {
+            String[] dateClickedParse = (cursor.getString(index0)).split(" ");
+            if(     dateClickedParse[0].contentEquals(parser[0]) &&
+                    dateClickedParse[1].contentEquals(parser[1]) &&
+                    dateClickedParse[2].contentEquals(parser[2])) {
+
                 String title = cursor.getString(index1);
                 String timeone = cursor.getString(index2);
                 String timetwo = cursor.getString(index3);
                 String message = cursor.getString(index4);
                 String color = cursor.getString(index5);
 
-                String s = dateDatabase + " " + title + " " + timeone + " " + timetwo + " " + message + " " + color;
-                mArrayList.add(s);
+                String[] s = new String[5];
+                s[0] = title;
+                s[1] = timeone;
+                s[2] = timetwo;
+                s[3] = message;
+                s[4] = color;
 
+                mArrayList.add(s);
             }
             cursor.moveToNext();
+            }
+
+            organizeEvents(mArrayList);
         }
-        for(int i = 0; i < mArrayList.size(); i++) {
-            Log.d(TAG, "TESTING " + mArrayList.get(i));
+
+    private void organizeEvents(ArrayList<String[]> mArrayList) {
+        int dp = Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 20, r.getDisplayMetrics()));
+
+        int AMorPM;
+
+        //EVENT LAYOUTS
+
+        LinearLayout.LayoutParams rlp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+        TextView event;
+        TextView eventTime;
+        TextView eventDesc;
+
+        for (int i = 0; i < mArrayList.size(); i++) {
+            Log.d(TAG, "TESTING " + mArrayList.get(i)[0] + " " + mArrayList.get(i)[1] + " " + mArrayList.get(i)[2] + " " + mArrayList.get(i)[3] + " " + mArrayList.get(i)[4]);
+
+            //Single event Layout
+            eventLayout = new LinearLayout(this);
+            if (mArrayList.get(i)[4].contentEquals("@colors/boxColor1")) {
+                eventLayout.setBackground(ContextCompat.getDrawable(this, R.drawable.textview_blue));
+            }
+            else if (mArrayList.get(i)[4].contentEquals("@colors/boxColor2")) {
+                eventLayout.setBackground(ContextCompat.getDrawable(this, R.drawable.textview_red));
+            }
+            else if (mArrayList.get(i)[4].contentEquals("@colors/boxColor3")) {
+                eventLayout.setBackground(ContextCompat.getDrawable(this, R.drawable.textview_yellow));
+            }
+            else if (mArrayList.get(i)[4].contentEquals("@colors/boxColor4")) {
+                eventLayout.setBackground(ContextCompat.getDrawable(this, R.drawable.textview_lightblue));
+            }
+            else if (mArrayList.get(i)[4].contentEquals("@colors/boxColor5")) {
+                eventLayout.setBackground(ContextCompat.getDrawable(this, R.drawable.textview_orange));
+            }
+            else {
+                eventLayout.setBackground(ContextCompat.getDrawable(this, R.drawable.textview_green));
+            }
+
+            int heightOfEvent = height * (((mArrayList.get(i)[2]).charAt(0) - 48) - ((mArrayList.get(i)[1]).charAt(0) - 48));
+            Log.d(TAG, "HEIGHT " + heightOfEvent);
+
+            LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            int marginTop = height * ((mArrayList.get(i)[1]).charAt(0) - 48);
+            lp.setMargins(0, marginTop, 0, 0);
+            lp.height = heightOfEvent;
+            eventLayout.setLayoutParams(lp);
+            eventLayout.setOrientation(LinearLayout.VERTICAL);
+            eventLayout.setPadding(dp, dp/3, dp, dp/3);
+
+            //Event Textviews
+            eventTime = new TextView(this);
+            event = new TextView(this);
+            eventDesc = new TextView(this);
+
+            eventTime.setLayoutParams(rlp);
+            eventTime.setTextColor(Color.WHITE);
+            eventTime.setTypeface(ResourcesCompat.getFont(this, R.font.circular_medium));
+            eventTime.setTextSize(10);
+            eventTime.setPadding(0, 0, 0, 30);
+            eventTime.setText(mArrayList.get(i)[1] + " " + mArrayList.get(i)[2]);
+
+            event.setLayoutParams(rlp);
+            event.setTextColor(Color.WHITE);
+            event.setTypeface(ResourcesCompat.getFont(this, R.font.circular_medium));
+            event.setTextSize(18);
+            event.setText(mArrayList.get(i)[0]);
+
+            eventDesc.setLayoutParams(rlp);
+            eventDesc.setTextColor(Color.WHITE);
+            eventDesc.setMaxWidth(width * 2);
+            eventDesc.setTypeface(ResourcesCompat.getFont(this, R.font.circular_book));
+            eventDesc.setTextSize(12);
+            eventDesc.setText(mArrayList.get(i)[3]);
+
+            eventLayout.addView(eventTime);
+            eventLayout.addView(event);
+            eventLayout.addView(eventDesc);
+
+            frameLayout.addView(eventLayout);
         }
+
+
     }
 }
