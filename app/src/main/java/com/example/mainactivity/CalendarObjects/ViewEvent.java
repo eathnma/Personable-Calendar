67c4024fa6305f91a1edfd47ebd087bf0b34faa9
Ethan <ethanmahc@gmail.com>
2020-03-27 17:23:31 -0700

    XML to View Activity finish

--- app/src/main/java/com/example/mainactivity/CalendarObjects/ViewEvent.java
+++ app/src/main/java/com/example/mainactivity/CalendarObjects/ViewEvent.java
@@ -3,10 +3,32 @@ package com.example.mainactivity.CalendarObjects;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.mainactivity.R;
 
 public class ViewEvent extends AppCompatActivity {
+
+    //layout row 1
+    private ImageView arrow;
+    private ImageView garbageButton;
+
+    //layout row 2
+    private ImageView circleView;
+    private TextView titleView;
+
+    //layout row 3
+    private TextView dateView;
+    private TextView timeView;
+    private TextView descriptionView;
+
+    //layout row 4
+    private TextView locationView;
+
+    //layout row 5
+    private View mapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
